60b6033aad84013e743c3161f4eb2449fa301174
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-01-01 15:23:35 +0530

    a

--- project/tictactoe.java
+++ project/tictactoe.java
@@ -1,2 +1,8 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
+class tictactoe{
+    public static void main(String[] args){
+        System.out.println("\t\t\tTIC-TAC-TOE GAME\t\t\t");
+    }
+}
