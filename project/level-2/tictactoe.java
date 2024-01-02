35db861b6b826e74e9774c02c3e687dea851d5a3
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-01-02 21:39:38 +0530

    added printRules function

--- project/level-2/tictactoe.java
+++ project/level-2/tictactoe.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 class tictactoe{
-    public static void main(String[] args){
+
+    static void printRules(){
         System.out.println("\t\t\tTIC-TAC-TOE GAME\t\t\t");
         System.out.print("RULES-FOR-THE-GAME:\n## The game is played on a 3x3 grid\n## Players take turns marking an empty cell with their respective symbols usually X and O by specifying the position of the cell\n");
         System.out.print("## The first player to get three of their symbols in a row, either horizontally, vertically, or diagonally, wins the game.\n## The game is over when all 9 squares are full.\n## The game of Tic Tac Toe ends in a tie when all nine squares are filled and neither player has three marked in a row");
-
-    } 
+    }
+    public static void main(String[] args){
+        printRules();
+    }
 }
