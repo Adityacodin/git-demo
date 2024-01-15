dde0a7b39d3e4ff12eba29aa9d3e8b88cb4e1022
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-01-15 22:07:18 +0530

    comment out unwanted imports

--- project/level-2/tictactoe.java
+++ project/level-2/tictactoe.java
@@ -1,9 +1,9 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
